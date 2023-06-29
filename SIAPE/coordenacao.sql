647a226898d6caa73531ca4615d71bac572760ac
Daniel Silva <136647220+0-Daniel@users.noreply.github.com>
2023-06-29 17:28:53 -0300

    Add files via upload

---
+++ SIAPE/coordenacao.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 29/06/2023 às 22:28
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `adm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `coordenacao`
+--
+
+CREATE TABLE `coordenacao` (
+  `cod_siape` int(7) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `curso` varchar(40) NOT NULL,
+  `telefone` int(14) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `endereco` varchar(150) NOT NULL,
+  `senha` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `coordenacao`
+--
+
+INSERT INTO `coordenacao` (`cod_siape`, `nome`, `curso`, `telefone`, `email`, `endereco`, `senha`) VALUES
+(9, 'jota', 'C.I Técnico em Informática', 451, 'juntosnomcpe@gmail.com', 'v', '1111');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `coordenacao`
+--
+ALTER TABLE `coordenacao`
+  ADD PRIMARY KEY (`cod_siape`),
+  ADD UNIQUE KEY `nome` (`nome`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `coordenacao`
+--
+ALTER TABLE `coordenacao`
+  MODIFY `cod_siape` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
