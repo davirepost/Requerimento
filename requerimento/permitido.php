c4f71c5a7b20c1edc08024bbdfc4873fd7387cd7
Davi Santos Souza <135336930+davirepost@users.noreply.github.com>
2023-08-18 10:17:52 -0300

    Update permitido.php

--- requerimento/permitido.php
+++ requerimento/permitido.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
      <!-- Jennifer -->
+     <!-- Esse código vai para uma página de cadastrado com sucesso-->
 </head>
 <body>
 
