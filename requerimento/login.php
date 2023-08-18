3777f70bbf96fd58b2849f4c9db12de58b44e0a3
Davi Santos Souza <135336930+davirepost@users.noreply.github.com>
2023-08-18 10:16:26 -0300

    Update login.php

--- requerimento/login.php
+++ requerimento/login.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
      <!-- Jennifer -->
+     <!-- Esse código faz o login dos discentes -->
 </head>
 <body>
 
