a7401929e3c4f40be3dd24efe16a242363856f59
Jennifer2601 <136204636+Jennifer2601@users.noreply.github.com>
2023-09-11 17:36:58 -0300

    Add files via upload

---
+++ Discente/dll.php
@@ -0,0 +1,25 @@
+ <!-- Esse código é a função para acessar e manipular o banco de dados-->
+<?php
+Function banco($server, $user, $password, $db, $consulta)
+	{
+		
+	 $banco =  new mysqli($server, $user, $password, $db); 
+	 if ($banco->connect_error) {
+            echo "Falha de conexão referência: (".$banco->connect_errno.") - ".$banco->connect_error;
+				echo "<a href = 'incluir.php'> <img src = 'img/fundo/voltar.png' / width = 40 heigth= 40> </a>";
+            exit();
+			
+	 }
+	 if (!$resultado = $banco->query($consulta)) {
+		        echo "Falha na consulta referência: (".$banco->errno.") - ".$banco->error;
+				  echo "<a href = 'incluir.php'> <img src = 'img/fundo/voltar.png' / width = 40 heigth= 40> </a>";
+				  exit();
+	 }
+    $banco->close();
+	 return $resultado;
+	
+	}
+
+	
+
+?>
