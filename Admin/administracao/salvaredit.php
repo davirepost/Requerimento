3a7a5b3adc4388ad73e088dc66bda451f735c6b7
GuilhermeRocha1360 <135336841+GuilhermeRocha1360@users.noreply.github.com>
2023-09-15 10:08:54 -0300

    Update salvaredit.php

--- Admin/administracao/salvaredit.php
+++ Admin/administracao/salvaredit.php
@@ -1,14 +1,14 @@
 <?php
     include ('../login/conexao.php');
-
+    extract($_POST);
     if (isset($alterar)) {
-        $cod_siape = $cod_siape;
+       
+        
         $nome = $nome;
-        $email = $email;
         $senha = $senha;
         $coordenacao = $coordenacao;
         // Atualize os valores no banco de dados
-        $consulta = "UPDATE coordenacao SET cod_siape='$cod_siape', nome='$nome', email='$email', senha='$senha', coordenacao='$coordenacao' WHERE id = $id";
+        $consulta = "UPDATE coordenacao SET  nome='$nome', senha='$senha', coordenacao='$coordenacao' WHERE id = $id";
         $resultado = banco($server, $user, $password, $db, $consulta);
         
     }
