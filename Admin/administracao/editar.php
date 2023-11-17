74bff7c380cb03e3a134d16170ed23aa5dddd11b
Davi Santos Souza <135336930+davirepost@users.noreply.github.com>
2023-11-16 23:29:07 -0300

    Update editar.php

--- Admin/administracao/editar.php
+++ Admin/administracao/editar.php
@@ -35,6 +35,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title>
+    <link rel="stylesheet" href="layout.css">
 </head>
 <body>
     <form action="salvaredit.php" method="post">
