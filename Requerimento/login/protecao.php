6bbe523cfae78134d1f18527f930b7fd2daedee8
Davi Santos Souza <135336930+davirepost@users.noreply.github.com>
2023-12-04 18:43:19 -0300

    Delete Requerimento directory

--- Requerimento/login/protecao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if(!isset($_SESSION)){
-        session_start();
-    }   
-
-    if (!isset($_SESSION['id'])){
-        die("Você não pode acessar está página, primeiro faça o seu login! <p> <a href= ../login/index.php> Voltar para a página de login </a> </p>");
-    }
-?>
